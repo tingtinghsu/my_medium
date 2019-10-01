eea258e96c77ad361ee552ea769ed168a00b8ba4
tingtinghsu <tingtinghsu.tw@gmail.com>
2019-10-01 22:31:29 +0900

    edit story using render form; update story with notification

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,10 @@
 class StoriesController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :find_story, only: [:edit, :update, :destoy]
+
+  def index
+    @stories = current_user.stories.order(updated_at: :desc)
+  end
   def new
     #@story = Story.new
     @story = current_user.stories.new
@@ -13,8 +18,28 @@ class StoriesController < ApplicationController
     end
   end
 
+  
+  def edit
+    
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to stories_path, notice: '故事更新成功！'
+    else
+      render :edit
+    end
+  end
+
+  def destoy
+    
+  end
   private
   def story_params
     params.require(:story).permit(:title, :content)
+  end
+
+  def find_story
+    @story = current_user.stories.find(params[:id])
   end
 end
